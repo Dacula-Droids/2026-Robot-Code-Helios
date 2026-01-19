8e983a1f1b762099186311a0acd0dd7bdb75d862
Sai Sathvik Avva <saisathvik.avva80@gmail.com>
2026-01-19 15:53:15 -0500

    update VisionConstants and Field Targets

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,11 +4,17 @@
 
 package frc.robot;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.util.Units;
 
 /** Add your docs here. */
@@ -28,11 +34,14 @@ public final class Constants {
     public final static int leftHubAprilTagID = 100;
     public final static int rightHubAprilTagID = 101;
 
-    //Arbitrary
-     public final static Pose3d leftHubPose = new Pose3d(new Translation3d(14.02, 2.03, 0), new Rotation3d(0,0,0)); //Left Tag, Zero for Robot to Face Hub
-     public final static Pose3d rightHubPose = new Pose3d(new Translation3d(14.02, 3.61, 0), new Rotation3d(0,0,0)); //Right Tag, Zero for Robot to Face Hub
-    
-     
+    //Exact Field Poses
+     public final static Pose3d leftHubPose = new Pose3d(new Translation3d(157.79,172.32,44.25), new Rotation3d(0,0,0)); //Left Tag, Zero for Robot to Face Hub
+     public final static Pose3d rightHubPose = new Pose3d(new Translation3d(157.79, 158.32, 44.25), new Rotation3d(0,0,0)); //Right Tag, Zero for Robot to Face Hub
+  }
 
+  public static class VisionConstants{
+    public static final Matrix<N3, N1> kSingleTagStdDevs = VecBuilder.fill(4,4,8);
+    public static final Matrix<N3, N1> kMultiTagStdDevs = VecBuilder.fill(0.5,0.5,1);
+    public static final AprilTagFieldLayout aprilTagFieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
   }
 }
