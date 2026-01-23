ef7c531b16c51b5cb9f2509884847b176c6bd1ed
CrypticXSystem <ryouk365@gmail.com>
2026-01-23 17:21:37 -0500

    add MaxSpeed and SwerveControllerDeadband constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,9 @@
 
 package frc.robot;
 
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.LinearVelocity;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -15,5 +18,10 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+    public static final double kSwerveControllerDeadband = 0.3;
+  }
+
+  public static class PhysicalConstants {
+    public static final LinearVelocity kMaxSpeed = Units.MetersPerSecond.of(4.5);
   }
 }
