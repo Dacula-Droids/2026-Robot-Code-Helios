72fd49bca21e569c25a9b92e138b6bdbdc0a6998
Sai Sathvik Avva <saisathvik.avva80@gmail.com>
2026-02-03 21:24:07 -0500

    modify RobotContainer
    Add controller control for drivetrain movement and buttonboard commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,67 +4,90 @@
 
 package frc.robot;
 
-import com.pathplanner.lib.auto.AutoBuilder;
-
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandGenericHID;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.OperatorConstants;
+import frc.robot.Utils.OutpostTarget;
 import frc.robot.subsystems.SwerveSubsystem;
 import swervelib.SwerveInputStream;
 
+/**
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
 public class RobotContainer {
- private SendableChooser<Command> autoChooser = new SendableChooser<>();
   // The robot's subsystems and commands are defined here...
-  public SwerveSubsystem swerveSubsystem = SwerveSubsystem.getInstance();
+  private final SwerveSubsystem swerveSubsystem = SwerveSubsystem.getInstance();
 
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return AutoBuilder.buildAuto("New Auto");
-  }
+  private final CommandXboxController driverXbox = new CommandXboxController(OperatorConstants.DRIVER_CONTROLLER_PORT);
+  private final CommandGenericHID buttonPad = new CommandGenericHID(OperatorConstants.buttonBoardPort);
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController driverController =
-      new CommandXboxController(OperatorConstants.DRIVER_CONTROLLER_PORT);
+  SwerveInputStream driveAngularVelocity = SwerveInputStream.of(swerveSubsystem.getSwerveDrive(),
+      () -> -driverXbox.getLeftY(),
+      () -> -driverXbox.getLeftX())
+      .withControllerRotationAxis(() -> -driverXbox.getRightX())
+      .deadband(OperatorConstants.SWERVE_DEADBAND)
+      .scaleTranslation(0.75).scaleRotation(0.5)
+      .allianceRelativeControl(false);
+  SwerveInputStream driveRobotOrientedAngularVelocity = driveAngularVelocity.copy().robotRelative(true);
 
-  SwerveInputStream driveAngularVelocity = SwerveInputStream.of(swerveSubsystem.getSwerveDrive(), () -> -driverController.getLeftY(), () -> -driverController.getLeftX()).withControllerRotationAxis(() -> driverController.getRightX())
-  .deadband(Constants.OperatorConstants.SWERVE_DEADBAND)
-  .scaleTranslation(0.3)
-  .scaleRotation(0.3)
-  .allianceRelativeControl(false);
-
-  
-
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
     // Configure the trigger bindings
-    autoChooser = AutoBuilder.buildAutoChooser();
-    SmartDashboard.putData("Auto Routine", autoChooser);
     configureBindings();
   }
 
-
   /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+   * an arbitrary
    * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link
+   * CommandXboxController
+   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
    * joysticks}.
    */
   private void configureBindings() {
-    Command driveFieldOrientedAngularVelocity = swerveSubsystem.driveFieldOriented(driveAngularVelocity);
-    swerveSubsystem.setDefaultCommand(driveFieldOrientedAngularVelocity);
+    Command driveFieldOrientedAnglularVelocity = swerveSubsystem.driveFieldOriented(driveAngularVelocity);
+    Command driveRobotOrientedAnglularVelocity = swerveSubsystem.driveFieldOriented(driveRobotOrientedAngularVelocity);
+    driverXbox.b().onTrue(
+        Commands.runOnce(() -> swerveSubsystem.zeroFieldOrientedHeading(driveAngularVelocity), swerveSubsystem));
+    // swerveSubsystem.setDefaultCommand(driveFieldOrientedAnglularVelocity);
+    swerveSubsystem.setDefaultCommand(driveRobotOrientedAnglularVelocity);
+
+    if (Robot.isSimulation()) {
+      driverXbox.a().onTrue(
+          Commands.runOnce(() -> swerveSubsystem.swerveDrive.resetOdometry(new Pose2d(7.6, 1.178, new Rotation2d()))));
+    }
+    
+    buttonPad.button(1).whileTrue(swerveSubsystem.pathFindToFieldTarget(OutpostTarget.Left, true));
+    
   }
 
-  
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
    * @return the command to run in autonomous
    */
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    return Commands.none();
+  }
+
 }
