8d912bd1f36d57f7d9948cbdc28417bcf872d4cc
ryouk <ryouk365@gmail.com>
2026-02-08 10:59:58 -0500

    fix Constants.java
    format correctly

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,11 +8,15 @@ import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.LinearVelocity;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
@@ -25,7 +29,7 @@ public final class Constants {
     public static final LinearVelocity kMaxSpeed = Units.MetersPerSecond.of(4.5);
   }
 
-  public static class IntakeConstants{
+  public static class IntakeConstants {
     public static final int intakeMotorID = 1;
     public static final int pivotMotorID = 2;
   }
