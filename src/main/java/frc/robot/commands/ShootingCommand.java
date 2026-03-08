3e948148a7a29813256e5cf53b5b75fe96f481d9
Sai Sathvik Avva <saisathvik.avva80@gmail.com>
2026-03-08 15:27:05 -0400

    add ShootingCommand
    Added in case needed, reduces redundant code

---
+++ src/main/java/frc/robot/commands/ShootingCommand.java
@@ -0,0 +1,28 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import static edu.wpi.first.units.Units.RPM;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.subsystems.IndexerSubsystem;
+import frc.robot.subsystems.KickerSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class ShootingCommand extends ParallelCommandGroup {
+  /** Creates a new ShootingCommand. */
+  public ShootingCommand(ShooterSubsystem shooterSubsystem, IndexerSubsystem indexerSubsystem,
+      KickerSubsystem kickerSubsystem) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addCommands(
+        shooterSubsystem
+            .run(() -> shooterSubsystem.setShooterFlywheelVelocitySetpoint(RPM.of(6.65 * 187.978279242 * 1.425))),
+        indexerSubsystem.setSpinDexerDutyCycle(0.5),
+        kickerSubsystem.setKickerDutyCycle(1));
+  }
+
+}
