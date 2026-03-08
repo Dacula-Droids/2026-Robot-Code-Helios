5ffa77bbf5b1f94f0bcd05f7cfc55497b12d8a39
Sai Sathvik Avva <saisathvik.avva80@gmail.com>
2026-03-08 15:31:21 -0400

    format TrenchTarget.java

--- src/main/java/frc/robot/Utils/TrenchTarget.java
+++ src/main/java/frc/robot/Utils/TrenchTarget.java
@@ -5,43 +5,61 @@ import frc.robot.Utils.FieldConstants;
 import frc.robot.Utils.FieldConstants.AprilTagIDs;
 
 public enum TrenchTarget implements FieldTarget {
-    
+
     LeftFront(AprilTagIDs.getAllianceTrenchLeftFrontId(),
-    FieldConstants.aprilTagFieldLayout.getTagPose(AprilTagIDs.getAllianceTrenchLeftFrontId()).get()){
+            FieldConstants.aprilTagFieldLayout.getTagPose(AprilTagIDs.getAllianceTrenchLeftFrontId()).get()) {
         @Override
-        public int getAprilTagId(){ return ApriltagId; } 
+        public int getAprilTagId() {
+            return ApriltagId;
+        }
+
         @Override
-        public Pose3d getTargetPose(){ return TargetPose; }
+        public Pose3d getTargetPose() {
+            return TargetPose;
+        }
     },
 
     RightFront(AprilTagIDs.getAllianceTrenchRightFrontId(),
-    FieldConstants.aprilTagFieldLayout.getTagPose(AprilTagIDs.getAllianceTrenchRightFrontId()).get()){
+            FieldConstants.aprilTagFieldLayout.getTagPose(AprilTagIDs.getAllianceTrenchRightFrontId()).get()) {
         @Override
-        public int getAprilTagId(){ return ApriltagId; } // Capital T
+        public int getAprilTagId() {
+            return ApriltagId;
+        } // Capital T
+
         @Override
-        public Pose3d getTargetPose(){ return TargetPose; }
+        public Pose3d getTargetPose() {
+            return TargetPose;
+        }
     },
     LeftBack(AprilTagIDs.getAllianceTrenchLeftBackId(),
-    FieldConstants.aprilTagFieldLayout.getTagPose(AprilTagIDs.getAllianceTrenchLeftBackId()).get()){
+            FieldConstants.aprilTagFieldLayout.getTagPose(AprilTagIDs.getAllianceTrenchLeftBackId()).get()) {
         @Override
-        public int getAprilTagId(){ return ApriltagId; } 
+        public int getAprilTagId() {
+            return ApriltagId;
+        }
+
         @Override
-        public Pose3d getTargetPose(){ return TargetPose; }
+        public Pose3d getTargetPose() {
+            return TargetPose;
+        }
     },
     RightBack(AprilTagIDs.getAllianceTrenchRightBackId(),
-    FieldConstants.aprilTagFieldLayout.getTagPose(AprilTagIDs.getAllianceTrenchRightBackId()).get()){
+            FieldConstants.aprilTagFieldLayout.getTagPose(AprilTagIDs.getAllianceTrenchRightBackId()).get()) {
         @Override
-        public int getAprilTagId(){ return ApriltagId; } 
+        public int getAprilTagId() {
+            return ApriltagId;
+        }
+
         @Override
-        public Pose3d getTargetPose(){ return TargetPose; }
+        public Pose3d getTargetPose() {
+            return TargetPose;
+        }
     };
 
-
-    
     public final int ApriltagId;
     public final Pose3d TargetPose;
 
-    private TrenchTarget(int ApriltagId, Pose3d TargetPose){
+    private TrenchTarget(int ApriltagId, Pose3d TargetPose) {
         this.ApriltagId = ApriltagId;
         this.TargetPose = TargetPose;
     }
