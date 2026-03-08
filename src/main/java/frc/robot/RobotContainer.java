3a81ff05496773465f192b49fbe17eda04640217
Sai Sathvik Avva <saisathvik.avva80@gmail.com>
2026-03-08 15:28:37 -0400

    add ShootingCommand
    reduces redundant code and allows for better readability

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -163,6 +163,22 @@ public class RobotContainer {
     // driverXbox.x().whileTrue(shooterSubsystem.sysIdDynamic(SysIdRoutine.Direction.kReverse));
   }
 
+  private Command getShootingCommand() {
+    return Commands.parallel(
+        shooterSubsystem
+            .run(() -> shooterSubsystem.setShooterFlywheelVelocitySetpoint(RPM.of(6.65 * 187.978279242 * 1.425))),
+        indexerSubsystem.setSpinDexerDutyCycle(0.5), kickerSubsystem.setKickerDutyCycle(1));
+
+  }
+
+  private Command stopShootingCommand() {
+    return Commands.parallel(
+        shooterSubsystem
+            .run(() -> shooterSubsystem.setShooterFlywheelVelocitySetpoint(RPM.of(0))),
+        indexerSubsystem.setSpinDexerDutyCycle(0), kickerSubsystem.setKickerDutyCycle(0));
+
+  }
+
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
