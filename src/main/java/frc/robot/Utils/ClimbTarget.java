0186802c2767696c282d77a5e10347803a1a41de
Sai Sathvik Avva <saisathvik.avva80@gmail.com>
2026-03-08 15:30:39 -0400

    format ClimbTarget.java

--- src/main/java/frc/robot/Utils/ClimbTarget.java
+++ src/main/java/frc/robot/Utils/ClimbTarget.java
@@ -5,26 +5,36 @@ import frc.robot.Utils.FieldConstants;
 import frc.robot.Utils.FieldConstants.AprilTagIDs;
 
 public enum ClimbTarget implements FieldTarget {
-    
+
     Left(AprilTagIDs.getAllianceClimbLeftId(),
-    FieldConstants.aprilTagFieldLayout.getTagPose(AprilTagIDs.getAllianceClimbLeftId()).get()){
+            FieldConstants.aprilTagFieldLayout.getTagPose(AprilTagIDs.getAllianceClimbLeftId()).get()) {
         @Override
-        public int getAprilTagId(){ return ApriltagId; } 
+        public int getAprilTagId() {
+            return ApriltagId;
+        }
+
         @Override
-        public Pose3d getTargetPose(){ return TargetPose; }
+        public Pose3d getTargetPose() {
+            return TargetPose;
+        }
     },
     Right(AprilTagIDs.getAllianceClimbRightId(),
-    FieldConstants.aprilTagFieldLayout.getTagPose(AprilTagIDs.getAllianceClimbRightId()).get()){
+            FieldConstants.aprilTagFieldLayout.getTagPose(AprilTagIDs.getAllianceClimbRightId()).get()) {
         @Override
-        public int getAprilTagId(){ return ApriltagId; } // Capital T
+        public int getAprilTagId() {
+            return ApriltagId;
+        } // Capital T
+
         @Override
-        public Pose3d getTargetPose(){ return TargetPose; }
+        public Pose3d getTargetPose() {
+            return TargetPose;
+        }
     };
-    
+
     public final int ApriltagId;
     public final Pose3d TargetPose;
 
-    private ClimbTarget(int ApriltagId, Pose3d TargetPose){
+    private ClimbTarget(int ApriltagId, Pose3d TargetPose) {
         this.ApriltagId = ApriltagId;
         this.TargetPose = TargetPose;
     }
